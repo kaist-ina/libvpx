7b6d8cc3dc00028b82dc9b558489019693c564c5
Hyunho Yeo <chaos5958@gmail.com>
2020-03-02 21:22:31 -0500

    Fix a bug

--- vp9/decoder/vp9_decodeframe.c
+++ vp9/decoder/vp9_decodeframe.c
@@ -3604,17 +3604,19 @@ void vp9_decode_frame(VP9Decoder *pbi, const uint8_t *data,
             cache_profile = get_cache_profile(cm->mobinas_cfg, cm->height);
 
             if (cm->frame_type == KEY_FRAME) {
+                if (read_cache_profile_dummy_bits(cache_profile) == -1) {
+                    fprintf(stderr, "%s: fall back to NO_CACHE mode", __func__);
+                    cm->mobinas_cfg->cache_policy = NO_CACHE;
+                    cm->apply_dnn = 0;
+                }
+                /*
                 //if first frame, reset cache profile
                 if(cm->current_video_frame % 8991 == 0){
                     cache_profile->offset = 0;
                     rewind(cache_profile->file);
                 }else{
-                    if (read_cache_profile_dummy_bits(cache_profile) == -1) {
-                        fprintf(stderr, "%s: fall back to NO_CACHE mode", __func__);
-                        cm->mobinas_cfg->cache_policy = NO_CACHE;
-                        cm->apply_dnn = 0;
-                    }
                 }
+                */
             }
 
 
